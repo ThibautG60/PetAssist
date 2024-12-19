ea22ea20892cbf9556df4e6de44c612953fee950
ThibautG60 <hatomalis@gmail.com>
2024-12-19 18:23:24 +0100

    Messagerie

--- assets/js/msg.js
+++ assets/js/msg.js
@@ -1,14 +1,37 @@
 const msgDial = document.getElementById('msg-dial');
-const msgButton = document.getElementById('msg-button');
+const msgButton = document.querySelectorAll('.msg-button');
 const closeMsgButton = document.getElementById('close-msg-button');
+const formMsg = document.getElementById('formMsg');
 
-msgButton.addEventListener("click", openMsg);
-closeMsgButton.addEventListener("click", closeMsg);
+formMsg.addEventListener('submit', addMsg);
 
-function openMsg() {
-    msgDial.showModal();
-}
-
-function closeMsg() {
+closeMsgButton.addEventListener("click", function () {
     msgDial.close();
+});
+
+msgButton.forEach(button => {
+    button.addEventListener('click', function () {
+        msgDial.showModal();
+    });
+});
+
+function addMsg(event) {
+    event.preventDefault();
+    let msgInput = document.getElementById("msg-input");
+    if (msgInput.classList.contains('form-error')) {
+        msgInput.classList.remove('form-error');
+    }
+    if (msgInput.value == "") {
+        msgInput.classList.add('form-error');
+        msgInput.placeholder = "Vous ne pouvez pas envoyer de message vide";
+    }
+    else {
+        let div = document.createElement('div');
+        div.classList.add('msg-text-1');
+        div.textContent = msgInput.value;
+        formMsg.insertAdjacentElement('beforebegin', div);
+        msgInput.value = "";
+        msgDial.scrollTop = msgDial.scrollHeight;
+    }
+    msgInput.focus();
 }
