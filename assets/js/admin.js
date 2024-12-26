80b0b41b4fdcb6cde16dbf3327e728a6a90f1e9d
ThibautG60 <hatomalis@gmail.com>
2024-12-26 12:06:58 +0100

    Modifications des caractères en mobile + up SEO

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,15 +4,21 @@ function showAdminIcon() {
     const iconAdminBox = document.querySelectorAll('.icon-admin-box');
 
     iconAdminBox.forEach(adminBox => {
+        let btnM = document.createElement("button");
+        let btnD = document.createElement("button");
         let imgModify = document.createElement("img");
         let imgDelete = document.createElement("img");
         imgModify.src = "assets/img/icons/modify.png";
         imgDelete.src = "assets/img/icons/delete.png";
         imgModify.classList.add('icon-admin');
         imgDelete.classList.add('icon-admin');
+        btnM.classList.add('btn-admin');
+        btnD.classList.add('btn-admin');
         imgModify.setAttribute("alt", "Icone administrateur");
         imgDelete.setAttribute("alt", "Icone administrateur");
-        adminBox.appendChild(imgModify);
-        adminBox.appendChild(imgDelete);
+        adminBox.appendChild(btnM);
+        adminBox.appendChild(btnD);
+        btnM.appendChild(imgModify);
+        btnD.appendChild(imgDelete);
     });
 }
